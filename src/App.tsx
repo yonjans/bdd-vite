d5cfd06f0483307b5348b508f85e4576ec7ccc37
sosuren <yonjan@truenary.com>
2023-10-24 00:39:29 +0545

    refactor: move config to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import Router from "./router";
+import { Config } from "./Config";
+import { todoRepository } from "@shared/contexts";
 
 const App = () => {
-  return <RouterProvider router={Router} />
+  return <Config todoRepository={todoRepository}>
+    <RouterProvider router={Router} />
+  </Config>
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Config } from './Config.tsx'
-import { todoRepository } from './shared/contexts.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Config todoRepository={todoRepository}>
-      <App />
-    </Config>
+    <App />
   </React.StrictMode>,
 )
